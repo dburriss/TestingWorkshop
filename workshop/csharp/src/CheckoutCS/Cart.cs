ccff4a79d375b2324e987ba4ad638957c34d65e7
dburriss <devon@chimplab.co>
2018-08-14 07:44:03 +0200

    Adds csharp workshop setup

---
+++ workshop/csharp/src/CheckoutCS/Cart.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace CheckoutCS
+{
+    public class Cart
+    {
+    }
+}
---
+++ workshop/csharp/test/CheckoutCS.Tests.Unit/CartTests.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace CheckoutCS.Tests.Unit
+{
+    public class CartTests
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
