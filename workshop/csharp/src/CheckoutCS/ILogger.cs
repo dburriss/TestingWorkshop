8116ba89d3e43de85278760c19af0d208cae52ad
dburriss <devon@chimplab.co>
2018-08-14 21:06:48 +0200

    Adds some models

---
+++ workshop/csharp/src/CheckoutCS/ILogger.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace CheckoutCS
+{
+    public interface ILogger
+    {
+        void Error(Exception ex);
+        void Information(string s);
+        void Trace(string s);
+    }
+}
---
+++ workshop/csharp/src/CheckoutCS/Product.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace CheckoutCS
+{
+    public class Product
+    {
+        public Guid Id { get; set; }
+        public string Code { get; set; }
+        public string Description { get; set; }
+        public decimal Amount { get; set; }
+        public long Version { get; set; }
+    }
+}
